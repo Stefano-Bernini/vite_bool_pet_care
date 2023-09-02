a6e92626b6355713fffa7a4ce435fc897ac32bdc
michelecanini <studio.michelecanini@gmail.com>
2023-09-02 02:06:34 +0200

    [TWEAK]  footer style

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -14,27 +14,32 @@ export default {
                 <div class="text-center">
                     <i class="fa-solid fa-paw fa-2xl"> BoolPetCare</i>
                 </div>
-                
-                <div class="col-sm-4">   
+                <div class="col-sm-3">   
                     <div class="player justify-content-end mt-5">
-                        
-                        <h4>Recapiti</h4>
-                        <h5>Via Degli Animali 17</h5> 
-                        <h5>00100 Roma</h5>
+                        <h5>Recapiti</h5>
+                        <h6>Via Degli Animali 17</h6> 
+                        <h6>00100 Roma</h6>
                     </div>
                 </div>
-                <div class="col-sm-4">   
+                <div class="col-sm-3">   
                     <div class="player justify-content-end mt-5">
-                        <h4>Contatti</h4>
-                        <h5>06 00000000</h5> 
-                        <h5>ambulatoriogatto@gmail.com</h5>
+                        <h5>Contatti</h5>
+                        <h6>06 00000000</h6> 
+                        <h6>ambulatoriogatto@gmail.com</h6>
                     </div>
                 </div>
-                <div class="col-sm-4">   
+                <div class="col-sm-3">   
                     <div class="player justify-content-end mt-5">
-                        <h4>Orari di Lavoro</h4>
-                        <h5>Lun - Ven  9:00 - 19:30</h5> 
-                        <h5>Sabato  9:00 - 15:00 </h5>
+                        <h5>Orari di Lavoro</h5>
+                        <h6>Lun - Ven  9:00 - 19:30</h6> 
+                        <h6>Sabato  9:00 - 15:00 </h6>
+                    </div>
+                </div>
+                <div class="col-sm-3">   
+                    <div class="player justify-content-end mt-5">
+                        <a href="#"><h5>Privacy Policy</h5></a>
+                        <a href="#"><h6>Cookie Policy</h6></a> 
+                        <a href="#"><h6>Termini e Condizioni</h6></a>
                     </div>
                 </div>
                 
