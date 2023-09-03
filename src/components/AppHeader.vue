c7db39f2bfa31e9d3299240ced980f10d3a4dac9
Stefano-Bernini <stefanobernini26@gmail.com>
2023-09-03 23:31:38 +0200

    header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,19 +5,29 @@ export default {
 </script>
 
 <template>
-    <div>
-        <div class="container">
-            <div class="row">
-                <div class="col-12 d-flex justify-content-between align-items-center">
-                    <div class="logo_bool_pet_care">
-                        <img src="" alt="">
-                    </div>
-                </div>
+    <nav class="navbar navbar-expand-lg mb-4">
+        <div class="container-fluid">
+            <a class="navbar-brand me-auto" href="#"><img src="../assets/site_logo.png" alt="" width="80px"></a>
+            <div class="d-flex justify-content-center w-100">
+                <i class="fa-solid fa-paw fa-2xl"> BoolPetCare</i>
+            </div>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active white-space" aria-current="page" href="#">Chi siamo</a>
+                    </li>
+                </ul>
             </div>
         </div>
-    </div>
+    </nav>
 </template>
 
 <style lang="scss" scoped>
-    
+    nav{
+        background-color: rgb(13, 202, 240)
+    }
+
+    .white-space{
+        white-space: nowrap;
+    }
 </style>
